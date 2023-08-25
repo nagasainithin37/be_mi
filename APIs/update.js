987a13565490997fbe73e106ced3bace14b31df1
nagasainithin37 <nagasainithin37@gmail.com>
2023-08-25 08:16:35 +0530

    admin group creation and deleteion completed

--- APIs/update.js
+++ APIs/update.js
@@ -5,6 +5,45 @@ const expressAsyncHandler=require('express-async-handler')
 const {ObjectId} = require('mongodb'); 
 
 
+updateApp.put('/addToGroup',expressAsyncHandler(async(req,res)=>{
+
+    const authCollection=req.app.get('authCollection')
+    const activeBatchCollection=req.app.get('activeBatchCollection')
+    const batchdetails=await activeBatchCollection.findOne({name:req.body.name})
+    const userIds=await authCollection.aggregate(
+        [
+            {
+                $match:{username:{$in:req.body.users}}
+            }
+            ,
+            {
+                $project:{_id:1}
+            }
+        ]
+    ).toArray()
+    var temp=[]
+    console.log(batchdetails)
+    console.log(userIds)
+    for(var i=0;i<userIds.length;i++){
+        var isPresent=false
+        for(var j=0;j<batchdetails.users.length;j++){
+            if(batchdetails.users[j].equals(userIds[i]['_id']))
+            {
+                isPresent=true
+                break
+            }
+        }
+        if(isPresent==false){
+            temp.push(userIds[i]['_id'])
+        }
+    }
+    await activeBatchCollection.updateMany({_id:batchdetails['_id']},{$set:{'users':[...batchdetails.users,...temp]}})
+    const update_result = await authCollection.updateMany({_id:{$in:temp}},{$push:{'batch':req.body.name}})
+    // console.log(update_result)
+    res.send({update_result})
+
+}))
+
 
 updateApp.put('/profiles',expressAsyncHandler(async(req,res)=>{
 
@@ -68,6 +107,52 @@ updateApp.put('/user',expressAsyncHandler(async(req,res)=>{
     res.send({message:'updated Successfully',userRes,profileRes,authRes})
 }))
 
+
+// updateApp.delete('/deleteUser',expressAsyncHandler(async(req,res)=>{
+
+// }))
+
+
+
+updateApp.get('/deleteBatch',expressAsyncHandler(async(req,res)=>{
+
+    console.log('hello')
+    const batch_name=req.query.name
+    const authCollection=req.app.get('authCollection')
+    const activeBatchCollection=req.app.get('activeBatchCollection')
+    const userCollection=req.app.get('userCollection')
+    const scoreCollection=req.app.get('scoreCollection')
+    const profileCollection=req.app.get('profileCollection')
+    const batchDetails=await activeBatchCollection.findOne({name:batch_name})
+    for(var i=0;i<batchDetails.users.length;i++){
+
+        const userAuth=await authCollection.findOne({_id:new ObjectId(batchDetails.users[i])})
+        if(userAuth.batch.length>1){
+            for(var j=0;j<userAuth.batch.length;j++){
+                if(userAuth.batch[j]==batch_name){
+                    userAuth.batch.splice(j,1)
+                    break
+                }
+            }
+           const data= await authCollection.updateOne({_id:userAuth._id},{$set:{batch:[...userAuth.batch]}})
+        }
+        else if(userAuth.batch.length==1){
+            const userObj=await userCollection.findOne({_id:new ObjectId(userAuth.userId)})
+            await profileCollection.deleteOne({_id:userObj.profileId})
+            const scoreIds=[]
+            for(var k=0;k<userObj.scoreId.length;k++){
+                scoreIds.push(userObj.scoreId[k]._id)
+            }
+            await scoreCollection.deleteMany({_id:{$in:scoreIds}})
+            await userCollection.deleteOne({_id:new ObjectId(userObj._id)})
+            await authCollection.deleteOne({_id:new ObjectId(userAuth._id)})
+        }
+    }
+    await activeBatchCollection.deleteOne({name:batch_name})
+    console.log('bye')
+    res.send({"message":"success"})
+
+}))
 
 
 module.exports=updateApp;
